cc263b0b5d11f389f56ae5c2aaa7e64810499368
RUAN LUIS MATOS BRANDAO SUAREZ <200026832@ucsal.br>
2023-09-06 08:55:23 -0300

    Adicionando Arquivos

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = 10
+var b = 5
+var c = 1
+var d
+
+c=--b
+b=++a
+a=5+"1"
+d="1"+"1"
+
+document.write("a:",a,"<br>")
+document.write("b:",b,"<br>")
+document.write("c:",c,"<br>")
+document.write("d:",d,"<br>")
+
+
